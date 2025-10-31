7f20e741ffe33e93d435a0266551a4a9f2265fca
Eduardo Martins <eduardo.martins@atmsistema.com.br>
2025-10-31 15:51:58 -0300

    Home screen and device screen

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import "./globals.css"
 import type { Metadata } from "next";
+
 import { Montserrat, Oxanium } from "next/font/google"
 
 export const metadata: Metadata = {
@@ -12,14 +13,24 @@ const oxanium = Oxanium({
   variable: "--font-oxanium"
 })
 
+const montserrat = Montserrat({
+  weight: ["400", "600"],
+  subsets: ["latin"],
+  variable: "--font-montserrat"
+})
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className="bg-black text-white">{children}</body>
+    <html lang="en" className={`${oxanium.variable} ${montserrat.variable}`}>
+      <body className="bg-gray-900 text-gray-100 antialiased bg-[url(/background.png)] bg-no-repeat bg-top md:bg-right-top">
+      <main className="max-w-[1240px] mx-auto px-5 py-8 md:py-0">
+        {children}
+        </main>
+      </body>
     </html>
   );
 }
