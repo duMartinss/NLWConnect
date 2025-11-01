bcf769b0afaae9823ea7f78c99063949963a2ecf
duMartinss <eduardo.martins22@aluno.senai.br>
2025-10-31 22:35:21 -0300

    fix

--- src/app/invite/invite-link-input.tsx
+++ src/app/invite/invite-link-input.tsx
@@ -1,26 +1,29 @@
-"use client"
+'use client'
 
-import { IconButton } from "@/components/icon-button";
-import { InputField, InputIcon, InputRoot } from "@/components/input";
-import { Copy, Link } from "lucide-react";
+import { Copy, Link } from 'lucide-react'
+import { IconButton } from '../../components/icon-button'
+import { InputField, InputIcon, InputRoot } from '../../components/input'
 
 interface InviteLinkInputProps {
-    InviteLink: string
+  inviteLink: string
 }
 
-export function InviteLinkInput({InviteLink}: InviteLinkInputProps) {
-    function copyInviteLink() {
-        navigator.clipboard.writeText(InviteLink)
-    }
-    return (
-        <InputRoot>
-            <InputIcon>
-                <Link className="size-5"/>
-            </InputIcon>
-            <InputField  readOnly defaultValue={InviteLink}/>
-            <IconButton className="-mr-2" onClick={copyInviteLink}>
-                <Copy className="size-5"/>
-            </IconButton>
-        </InputRoot>
-    )
+export function InviteLinkInput({ inviteLink }: InviteLinkInputProps) {
+  function copyInviteLink() {
+    navigator.clipboard.writeText(inviteLink)
+  }
+
+  return (
+    <InputRoot>
+      <InputIcon>
+        <Link className="size-5" />
+      </InputIcon>
+
+      <InputField readOnly defaultValue={inviteLink} />
+
+      <IconButton className="-mr-2" onClick={copyInviteLink}>
+        <Copy className="size-5" />
+      </IconButton>
+    </InputRoot>
+  )
 }
